6245bb323ead2ac3ea99536c2df9ecfb320d291d
Douglas Curtis <docurtis@redhat.com>
2018-10-24 15:55:45 -0400

    inital changes on base class

--- koku/api/report/aws/queries.py
+++ koku/api/report/aws/queries.py
@@ -35,6 +35,7 @@ from django.db.models.functions import (Concat,
                                         TruncMonth)
 from tenant_schemas.utils import tenant_context
 
+from api.report.base_query_handler import ReportQueryHandlerBase
 from api.report.query_filter import QueryFilter, QueryFilterCollection
 from api.utils import DateHelper
 from reporting.models import (AWSCostEntryLineItem,
@@ -43,7 +44,6 @@ from reporting.models import (AWSCostEntryLineItem,
 
 
 LOG = logging.getLogger(__name__)
-WILDCARD = '*'
 OPERATION_SUM = 'sum'
 OPERATION_NONE = 'none'
 EXPORT_COLUMNS = ['cost_entry_id', 'cost_entry_bill_id',
@@ -220,25 +220,7 @@ class ProviderMap(object):
         return self._report_type_map.get('units_key')
 
 
-class TruncDayString(TruncDay):
-    """Class to handle string formated day truncation."""
-
-    def convert_value(self, value, expression, connection):
-        """Convert value to a string after super."""
-        value = super().convert_value(value, expression, connection)
-        return value.strftime('%Y-%m-%d')
-
-
-class TruncMonthString(TruncMonth):
-    """Class to handle string formated day truncation."""
-
-    def convert_value(self, value, expression, connection):
-        """Convert value to a string after super."""
-        value = super().convert_value(value, expression, connection)
-        return value.strftime('%Y-%m')
-
-
-class AWSReportQueryHandler(object):
+class AWSReportQueryHandler(ReportQueryHandlerBase):
     """Handles report queries and responses."""
 
     default_ordering = {'total': 'desc'}
@@ -255,28 +237,6 @@ class AWSReportQueryHandler(object):
         """
         LOG.debug(f'Query Params: {query_parameters}')
 
-        self._accept_type = None
-        self._annotations = None
-        self._group_by = None
-        self.end_datetime = None
-        self.resolution = None
-        self.start_datetime = None
-        self.time_interval = []
-        self.time_scope_units = None
-        self.time_scope_value = None
-
-        self.query_parameters = query_parameters
-        self.tenant = tenant
-        self.url_data = url_data
-
-        self.provider_name = self.query_parameters.get('provider', 'AWS')
-        self.operation = self.query_parameters.get('operation', OPERATION_SUM)
-
-        self._delta = self.query_parameters.get('delta')
-        self._limit = self.get_query_param_data('filter', 'limit')
-        self._get_timeframe()
-        self.query_delta = {'value': None, 'percent': None}
-
         if kwargs:
             elements = ['accept_type', 'annotations', 'delta',
                         'group_by', 'report_type']
@@ -287,9 +247,27 @@ class AWSReportQueryHandler(object):
         assert getattr(self, '_report_type'), \
             'kwargs["report_type"] is missing!'
 
+        self.provider_name = query_parameters.get('provider', 'AWS')
+        self.operation = query_parameters.get('operation', OPERATION_SUM)
+
         self._mapper = ProviderMap(provider=self.provider_name,
                                    operation=self.operation,
                                    report_type=self._report_type)
+
+        super().__init__(query_parameters, url_data,
+                 tenant, self.default_ordering, self._mapper, **kwargs)
+
+        self._accept_type = None
+        self._annotations = None
+        self._group_by = None
+        self.time_interval = []
+
+        self._group_by_options = ['service', 'account', 'region', 'avail_zone']
+        self._delta = self.query_parameters.get('delta')
+        self._limit = self.get_query_param_data('filter', 'limit')
+        self.query_delta = {'value': None, 'percent': None}
+
+
         self.query_filter = self._get_filter()
 
     @property
@@ -301,225 +279,6 @@ class AWSReportQueryHandler(object):
 
         """
         return self.operation == OPERATION_SUM
-
-    @staticmethod
-    def has_wildcard(in_list):
-        """Check if list has wildcard.
-
-        Args:
-            in_list (List[String]): List of strings to check for wildcard
-        Return:
-            (Boolean): if wildcard is present in list
-        """
-        if not in_list:
-            return False
-        return any(WILDCARD == item for item in in_list)
-
-    def check_query_params(self, key, in_key):
-        """Test if query parameters has a given key and key within it.
-
-        Args:
-        key     (String): key to check in query parameters
-        in_key  (String): key to check if key is found in query parameters
-
-        Returns:
-            (Boolean): True if they keys given appear in given query parameters.
-
-        """
-        return (self.query_parameters and key in self.query_parameters and in_key in self.query_parameters.get(key))
-
-    def get_query_param_data(self, dictkey, key, default=None):
-        """Extract the value from a query parameter dictionary or return None.
-
-        Args:
-            dictkey (String): the key to access a query parameter dictionary
-            key     (String): the key to obtain from the dictionar data
-        Returns:
-            (Object): The value found with the given key or the default value
-        """
-        value = default
-        if self.check_query_params(dictkey, key):
-            value = self.query_parameters.get(dictkey).get(key)
-        return value
-
-    @property
-    def order_field(self):
-        """Order-by field name.
-
-        The default is 'total'
-        """
-        order_by = self.query_parameters.get('order_by', self.default_ordering)
-        return list(order_by.keys()).pop()
-
-    @property
-    def order_direction(self):
-        """Order-by orientation value.
-
-        Returns:
-            (str) 'asc' or 'desc'; default is 'desc'
-
-        """
-        order_by = self.query_parameters.get('order_by', self.default_ordering)
-        return list(order_by.values()).pop()
-
-    @property
-    def order(self):
-        """Extract order_by parameter and apply ordering to the appropriate field.
-
-        Returns:
-            (String): Ordering value. Default is '-total'
-
-        Example:
-            `order_by[total]=asc` returns `total`
-            `order_by[total]=desc` returns `-total`
-
-        """
-        order_map = {'asc': '', 'desc': '-'}
-        return f'{order_map[self.order_direction]}{self.order_field}'
-
-    def get_resolution(self):
-        """Extract resolution or provide default.
-
-        Returns:
-            (String): The value of how data will be sliced.
-
-        """
-        if self.resolution:
-            return self.resolution
-
-        self.resolution = self.get_query_param_data('filter', 'resolution')
-        time_scope_value = self.get_time_scope_value()
-        if not self.resolution:
-            self.resolution = 'daily'
-            if int(time_scope_value) in [-1, -2]:
-                self.resolution = 'monthly'
-
-        if self.resolution == 'monthly':
-            self.date_to_string = lambda dt: dt.strftime('%Y-%m')
-            self.string_to_date = lambda dt: datetime.datetime.strptime(dt, '%Y-%m').date()
-            self.date_trunc = TruncMonthString
-            self.gen_time_interval = DateHelper().list_months
-        else:
-            self.date_to_string = lambda dt: dt.strftime('%Y-%m-%d')
-            self.string_to_date = lambda dt: datetime.datetime.strptime(dt, '%Y-%m-%d').date()
-            self.date_trunc = TruncDayString
-            self.gen_time_interval = DateHelper().list_days
-
-        return self.resolution
-
-    def get_time_scope_units(self):
-        """Extract time scope units or provide default.
-
-        Returns:
-            (String): The value of how data will be sliced.
-
-        """
-        if self.time_scope_units:
-            return self.time_scope_units
-
-        time_scope_units = self.get_query_param_data('filter', 'time_scope_units')
-        time_scope_value = self.get_query_param_data('filter', 'time_scope_value')
-        if not time_scope_units:
-            time_scope_units = 'day'
-            if time_scope_value and int(time_scope_value) in [-1, -2]:
-                time_scope_units = 'month'
-
-        self.time_scope_units = time_scope_units
-        return self.time_scope_units
-
-    def get_time_scope_value(self):
-        """Extract time scope value or provide default.
-
-        Returns:
-            (Integer): time relative value providing query scope
-
-        """
-        if self.time_scope_value:
-            return self.time_scope_value
-
-        time_scope_units = self.get_query_param_data('filter', 'time_scope_units')
-        time_scope_value = self.get_query_param_data('filter', 'time_scope_value')
-
-        if not time_scope_value:
-            time_scope_value = -10
-            if time_scope_units == 'month':
-                time_scope_value = -1
-
-        self.time_scope_value = int(time_scope_value)
-        return self.time_scope_value
-
-    def _create_time_interval(self):
-        """Create list of date times in interval.
-
-        Returns:
-            (List[DateTime]): List of all interval slices by resolution
-
-        """
-        self.time_interval = sorted(self.gen_time_interval(
-            self.start_datetime,
-            self.end_datetime))
-        return self.time_interval
-
-    def _get_timeframe(self):
-        """Obtain timeframe start and end dates.
-
-        Returns:
-            (DateTime): start datetime for query filter
-            (DateTime): end datetime for query filter
-
-        """
-        self.get_resolution()
-        time_scope_value = self.get_time_scope_value()
-        time_scope_units = self.get_time_scope_units()
-        start = None
-        end = None
-        dh = DateHelper()
-        if time_scope_units == 'month':
-            if time_scope_value == -1:
-                # get current month
-                start = dh.this_month_start
-                end = dh.this_month_end
-            else:
-                # get previous month
-                start = dh.last_month_start
-                end = dh.last_month_end
-        else:
-            if time_scope_value == -10:
-                # get last 10 days
-                start = dh.n_days_ago(dh.this_hour, 10)
-                end = dh.this_hour
-            else:
-                # get last 30 days
-                start = dh.n_days_ago(dh.this_hour, 30)
-                end = dh.this_hour
-
-        self.start_datetime = start
-        self.end_datetime = end
-        self._create_time_interval()
-        return (self.start_datetime, self.end_datetime, self.time_interval)
-
-    def _get_search_filter(self, filters):
-        """Populate the query filter collection for search filters.
-
-        Args:
-            filters (QueryFilterCollection): collection of query filters
-        Returns:
-            (QueryFilterCollection): populated collection of query filters
-        """
-        # define filter parameters using API query params.
-        fields = self._mapper._operation_map.get('filters')
-        for q_param, filt in fields.items():
-            group_by = self.get_query_param_data('group_by', q_param, list())
-            filter_ = self.get_query_param_data('filter', q_param, list())
-            list_ = list(set(group_by + filter_))    # uniquify the list
-            if list_ and not AWSReportQueryHandler.has_wildcard(list_):
-                for item in list_:
-                    q_filter = QueryFilter(parameter=item, **filt)
-                    filters.add(q_filter)
-
-        composed_filters = filters.compose()
-        LOG.debug(f'_get_search_filter: {composed_filters}')
-        return composed_filters
 
     def _get_filter(self, delta=False):
         """Create dictionary for filter parameters.
@@ -534,7 +293,6 @@ class AWSReportQueryHandler(object):
 
         # set up filters for instance-type and storage queries.
         filters.add(**self._mapper._report_type_map.get('filter'))
-
         if delta:
             if self.time_scope_value in [-1, -2]:
                 date_delta = relativedelta.relativedelta(months=1)
@@ -564,7 +322,7 @@ class AWSReportQueryHandler(object):
     def _get_group_by(self):
         """Create list for group_by parameters."""
         group_by = []
-        group_by_options = ['service', 'account', 'region', 'avail_zone']
+        group_by_options = self._group_by_options
         for item in group_by_options:
             group_data = self.get_query_param_data('group_by', item)
             if group_data:
@@ -761,7 +519,7 @@ class AWSReportQueryHandler(object):
             query_order_by = ('-date', )
             if self.order_field != 'delta':
                 query_order_by += (self.order,)
-
+            import pdb; pdb.set_trace()
             aggregate_key = self._mapper._report_type_map.get('aggregate_key')
             query_data = query_data.values(*query_group_by)\
                 .annotate(total=Sum(aggregate_key))\
---
+++ koku/api/report/base_query_handler.py
@@ -0,0 +1,297 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Base Query Handling for Reports."""
+import datetime
+import logging
+
+from django.db.models.functions import (Concat,
+                                        DenseRank,
+                                        TruncDay,
+                                        TruncMonth)
+
+from api.utils import DateHelper
+
+from api.report.query_filter import QueryFilter, QueryFilterCollection
+
+WILDCARD = '*'
+LOG = logging.getLogger(__name__)
+
+
+class TruncDayString(TruncDay):
+    """Class to handle string formated day truncation."""
+
+    def convert_value(self, value, expression, connection):
+        """Convert value to a string after super."""
+        value = super().convert_value(value, expression, connection)
+        return value.strftime('%Y-%m-%d')
+
+
+class TruncMonthString(TruncMonth):
+    """Class to handle string formated day truncation."""
+
+    def convert_value(self, value, expression, connection):
+        """Convert value to a string after super."""
+        value = super().convert_value(value, expression, connection)
+        return value.strftime('%Y-%m')
+
+
+class ReportQueryHandlerBase(object):
+    """Handles report queries and responses."""
+
+    default_ordering = {'total': 'desc'}
+
+    def __init__(self, query_parameters, url_data,
+                 tenant, default_ordering, mapper, **kwargs):
+        """Establish report query handler.
+
+        Args:
+            query_parameters    (Dict): parameters for query
+            url_data        (String): URL string to provide order information
+            tenant    (String): the tenant to use to access CUR data
+            kwargs    (Dict): A dictionary for internal query alteration based on path
+        """
+
+        self.query_parameters = query_parameters
+        self.default_ordering = default_ordering
+        self.url_data = url_data
+        self.tenant = tenant
+        self._mapper = mapper
+
+        self.resolution = None
+        self.time_scope_units = None
+        self.time_scope_value = None
+
+        self._get_timeframe()
+
+    @staticmethod
+    def has_wildcard(in_list):
+        """Check if list has wildcard.
+
+        Args:
+            in_list (List[String]): List of strings to check for wildcard
+        Return:
+            (Boolean): if wildcard is present in list
+        """
+        if not in_list:
+            return False
+        return any(WILDCARD == item for item in in_list)
+
+    @property
+    def order_field(self):
+        """Order-by field name.
+
+        The default is 'total'
+        """
+        order_by = self.query_parameters.get('order_by', self.default_ordering)
+        return list(order_by.keys()).pop()
+
+    @property
+    def order_direction(self):
+        """Order-by orientation value.
+
+        Returns:
+            (str) 'asc' or 'desc'; default is 'desc'
+
+        """
+        order_by = self.query_parameters.get('order_by', self.default_ordering)
+        return list(order_by.values()).pop()
+
+    @property
+    def order(self):
+        """Extract order_by parameter and apply ordering to the appropriate field.
+
+        Returns:
+            (String): Ordering value. Default is '-total'
+
+        Example:
+            `order_by[total]=asc` returns `total`
+            `order_by[total]=desc` returns `-total`
+
+        """
+        order_map = {'asc': '', 'desc': '-'}
+        return f'{order_map[self.order_direction]}{self.order_field}'
+
+    def check_query_params(self, key, in_key):
+        """Test if query parameters has a given key and key within it.
+
+        Args:
+        key     (String): key to check in query parameters
+        in_key  (String): key to check if key is found in query parameters
+
+        Returns:
+            (Boolean): True if they keys given appear in given query parameters.
+
+        """
+        return (self.query_parameters and key in self.query_parameters and in_key in self.query_parameters.get(key))
+
+    def get_query_param_data(self, dictkey, key, default=None):
+        """Extract the value from a query parameter dictionary or return None.
+
+        Args:
+            dictkey (String): the key to access a query parameter dictionary
+            key     (String): the key to obtain from the dictionar data
+        Returns:
+            (Object): The value found with the given key or the default value
+        """
+        value = default
+        if self.check_query_params(dictkey, key):
+            value = self.query_parameters.get(dictkey).get(key)
+        return value
+    
+    def _get_search_filter(self, filters):
+        """Populate the query filter collection for search filters.
+
+        Args:
+            filters (QueryFilterCollection): collection of query filters
+        Returns:
+            (QueryFilterCollection): populated collection of query filters
+        """
+        # define filter parameters using API query params.
+        fields = self._mapper._operation_map.get('filters')
+        for q_param, filt in fields.items():
+            group_by = self.get_query_param_data('group_by', q_param, list())
+            filter_ = self.get_query_param_data('filter', q_param, list())
+            list_ = list(set(group_by + filter_))    # uniquify the list
+            if list_ and not ReportQueryHandlerBase.has_wildcard(list_):
+                for item in list_:
+                    q_filter = QueryFilter(parameter=item, **filt)
+                    filters.add(q_filter)
+
+        composed_filters = filters.compose()
+        LOG.debug(f'_get_search_filter: {composed_filters}')
+        return composed_filters
+
+    def get_time_scope_value(self):
+        """Extract time scope value or provide default.
+
+        Returns:
+            (Integer): time relative value providing query scope
+
+        """
+        if self.time_scope_value:
+            return self.time_scope_value
+
+        time_scope_units = self.get_query_param_data('filter', 'time_scope_units')
+        time_scope_value = self.get_query_param_data('filter', 'time_scope_value')
+
+        if not time_scope_value:
+            time_scope_value = -10
+            if time_scope_units == 'month':
+                time_scope_value = -1
+
+        self.time_scope_value = int(time_scope_value)
+        return self.time_scope_value
+
+    def get_resolution(self):
+        """Extract resolution or provide default.
+
+        Returns:
+            (String): The value of how data will be sliced.
+
+        """
+        if self.resolution:
+            return self.resolution
+
+        self.resolution = self.get_query_param_data('filter', 'resolution')
+        time_scope_value = self.get_time_scope_value()
+        if not self.resolution:
+            self.resolution = 'daily'
+            if int(time_scope_value) in [-1, -2]:
+                self.resolution = 'monthly'
+
+        if self.resolution == 'monthly':
+            self.date_to_string = lambda dt: dt.strftime('%Y-%m')
+            self.string_to_date = lambda dt: datetime.datetime.strptime(dt, '%Y-%m').date()
+            self.date_trunc = TruncMonthString
+            self.gen_time_interval = DateHelper().list_months
+        else:
+            self.date_to_string = lambda dt: dt.strftime('%Y-%m-%d')
+            self.string_to_date = lambda dt: datetime.datetime.strptime(dt, '%Y-%m-%d').date()
+            self.date_trunc = TruncDayString
+            self.gen_time_interval = DateHelper().list_days
+
+        return self.resolution
+
+    def get_time_scope_units(self):
+        """Extract time scope units or provide default.
+
+        Returns:
+            (String): The value of how data will be sliced.
+
+        """
+        if self.time_scope_units:
+            return self.time_scope_units
+
+        time_scope_units = self.get_query_param_data('filter', 'time_scope_units')
+        time_scope_value = self.get_query_param_data('filter', 'time_scope_value')
+        if not time_scope_units:
+            time_scope_units = 'day'
+            if time_scope_value and int(time_scope_value) in [-1, -2]:
+                time_scope_units = 'month'
+
+        self.time_scope_units = time_scope_units
+        return self.time_scope_units
+
+    def _create_time_interval(self):
+        """Create list of date times in interval.
+
+        Returns:
+            (List[DateTime]): List of all interval slices by resolution
+
+        """
+        self.time_interval = sorted(self.gen_time_interval(
+            self.start_datetime,
+            self.end_datetime))
+        return self.time_interval
+
+    def _get_timeframe(self):
+        """Obtain timeframe start and end dates.
+
+        Returns:
+            (DateTime): start datetime for query filter
+            (DateTime): end datetime for query filter
+
+        """
+        self.get_resolution()
+        time_scope_value = self.get_time_scope_value()
+        time_scope_units = self.get_time_scope_units()
+        start = None
+        end = None
+        dh = DateHelper()
+        if time_scope_units == 'month':
+            if time_scope_value == -1:
+                # get current month
+                start = dh.this_month_start
+                end = dh.this_month_end
+            else:
+                # get previous month
+                start = dh.last_month_start
+                end = dh.last_month_end
+        else:
+            if time_scope_value == -10:
+                # get last 10 days
+                start = dh.n_days_ago(dh.this_hour, 10)
+                end = dh.this_hour
+            else:
+                # get last 30 days
+                start = dh.n_days_ago(dh.this_hour, 30)
+                end = dh.this_hour
+
+        self.start_datetime = start
+        self.end_datetime = end
+        self._create_time_interval()
+        return (self.start_datetime, self.end_datetime, self.time_interval)
